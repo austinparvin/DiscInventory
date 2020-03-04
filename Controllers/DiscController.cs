62e6bcc475d1eaff914238cf2ecb4905b09c69e9
Austin Parvin <austin.parvin@gmail.com>
2020-03-04 14:05:40 -0500

    added 3 Api endpoints; 2 get 1 put

--- Controllers/DiscController.cs
+++ Controllers/DiscController.cs
@@ -20,5 +20,25 @@ namespace DiscInventory.Controllers
         {
             return await db.Discs.OrderBy(d => d.Name).ToListAsync();
         }
+        [HttpGet("id/{id}")]
+        public async Task<Disc> GetDiscById(int id)
+        {
+            return await db.Discs.FirstOrDefaultAsync(d => d.Id == id);
+        }
+        [HttpPost]
+        public async Task<Disc> AddDiscToInventory(Disc disc)
+        {
+            await db.Discs.AddAsync(disc);
+            await db.SaveChangesAsync();
+            return disc;
+        }
+        [HttpPut("{id}")]
+        public async Task<Disc> UpdateDiscInInventory(int id, Disc newDiscData)
+        {
+            newDiscData.Id = id;
+            db.Entry(newDiscData).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return newDiscData;
+        }
     }
 }
